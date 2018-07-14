24cdef18dfca49facacf9b0eea771d453a3c4fcb
Luis Rodolfo <rodolfolarae@gmail.com>
2018-07-14 09:26:58 -0500

    Login y registro de usuarios en el Frontend terminado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 // Rutas
 import { APP_ROUTING } from './app.routes';
@@ -28,6 +28,7 @@ import { ServiceModule } from './services/service.module';
     APP_ROUTING,
     PagesModule,
     FormsModule,
+    ReactiveFormsModule,
     ServiceModule
   ],
   providers: [],
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { NgForm } from '@angular/forms';
+import { UsuarioService } from '../services/service.index';
+import { Usuario } from '../models/usuario.model';
 
 declare function init_plugins();
+declare const gapi: any;
 
 @Component({
   selector: 'app-login',
@@ -9,16 +13,57 @@ declare function init_plugins();
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  email: string;
+  recuerdame = false;
+  auth2: any;
 
-  constructor( public router: Router) { }
+  constructor(
+    public router: Router,
+    public _usuarioService: UsuarioService
+  ) { }
 
   ngOnInit() {
-  	init_plugins();
+    init_plugins();
+    this.googleInit();
+    this.email = localStorage.getItem('email') || '';
+    if ( this.email.length > 1 ) {
+      this.recuerdame = true;
+    }
   }
 
-  ingresar(){
-  	console.log('Ingresando')
-  	this.router.navigate(['/dashboard'])
+  googleInit() {
+    gapi.load('auth2', () => {
+      this.auth2 = gapi.auth2.init({
+        client_id: '383996623861-ktc5sar1eaerg9ddrmok7uje6ihar43t.apps.googleusercontent.com',
+        cookiepolicy: 'single_host_origin',
+        scope: 'profile email'
+      });
+
+      this.attachSignin( document.getElementById('btnGoogle'));
+    });
+  }
+
+  attachSignin( element ) {
+    this.auth2.attachClickHandler( element, {}, (googleUser) => {
+      // const profile = googleUser.getBasicProfile();
+      const token = googleUser.getAuthResponse().id_token;
+
+      this._usuarioService.loginGoogle( token )
+        .subscribe( () => window.location.href = '#/dashboard');
+      console.log(token);
+    });
+  }
+
+  ingresar( forma: NgForm) {
+
+    if( forma.invalid ) {
+      return;
+    }
+
+    const usuario = new Usuario(null, forma.value.email, forma.value.password);
+
+    this._usuarioService.login( usuario, forma.value.recuerdame )
+        .subscribe( correcto => this.router.navigate(['/dashboard']));
   }
 
 }
